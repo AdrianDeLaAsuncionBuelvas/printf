0f5d72bc7a23a6a0b05af4d67a7ae23da34aa797
diego <diego>
2020-03-15 17:58:24 -0500

    1

---
+++ print.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void print(char *f)
+{
+    int i; 
+    for (i = 0; f[i] != 0; i++)
+    {
+    if (f[i] == '%' || f[i] == '/')
+    {
+    putchar('e');
+    continue;
+    }
+    
+    else
+    {
+    write(1, f+i, 1);
+    }
+
+    
+    
+    }
+    
+}
+
+
+
+
+int main()
+{
+    print("Hello World%");
+
+    return 0;
+}
+
